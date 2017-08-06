a171f6890fc18cbb63158b5119986eaa4e39f80d
sharad joe <sharadjoe48@gmail.com>
2017-08-06 21:51:17 +0530

    third or forth commit an 1-d error occured

--- my_classifier_creation.py
+++ my_classifier_creation.py
@@ -1,10 +1,29 @@
+from scipy.spatial import distance
+
+def euc(a, b):
+    return distance.euclidean(a, b)
+
 class scrapyknn():
     def fit(self, x_train, y_train):
         self.x_train = x_train
         self.y_train = y_train
 
     def predict(self, x_test):
-        pass
+        predictions = []
+        for row in x_test:
+            label = self.closest(row)
+            predictions.append(label)
+
+        return predictions
+    def closest(self, row):
+        best_distance = euc(row, self.x_train)
+        best_index = 0
+        for i in range(1, len(self.x_train)):
+            dist = euc(row, self.x_train)
+            if dist<best_distance:
+                best_distance = dist
+                best_index = i
+        return self.y_train[best_index]
     
 
 
