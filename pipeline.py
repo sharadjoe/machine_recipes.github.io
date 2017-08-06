2be235be35babba9d456f4a397c2b655acb644a8
sharad joe <sharadjoe48@gmail.com>
2017-08-06 18:27:16 +0530

    second commit

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,6 @@
 #import a dataset
-from sklearn import datasets
-iris = datasets.load_iris()
+from sklearn.datasets import load_iris
+iris = load_iris()
 
 
 x = iris.data
@@ -10,4 +10,21 @@ y = iris.target
 from sklearn.cross_validation import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.5)
 
-#classifier creation
+#classifier creation number 1
+#from sklearn import tree
+#my_classifier = tree.DecisionTreeClassifier()
+
+#classifier creation number 2
+from sklearn.neighbors import KNeighborsClassifier
+my_classifier = KNeighborsClassifier()  
+
+
+my_classifier.fit(x_train, y_train)
+
+#using predictions
+predictions = my_classifier.predict(x_test)
+
+
+#measuring accuracy
+from sklearn.metrics import accuracy_score
+print accuracy_score(y_test, predictions)
